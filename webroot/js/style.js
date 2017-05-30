b97074099658920370411fe0eadf1077a59a1760
Ivan Bechev <ivan_bechev@yahoo.com>
2017-05-30 12:28:47 +0100

    Composer.json, Services Page
    - Added Composer.json
    - Started to style Services pahe

--- webroot/js/style.js
+++ webroot/js/style.js
@@ -1,18 +1,32 @@
+// Set min height for the HOME section
+var setHomeHeight = function() {	
+	var docHeight = $(window).height();
+	$('section.home').css('height', docHeight + 'px');
+	$('section.index:not(home)').css('min-height', docHeight + 'px');
+}
+
+var logoCenterVert = function() {
+	//  Set the Logo on the Home page to be vertically centered
+	var logoHome = $('section.home').find('div.logo-wrapper');
+	logoHome.css('margin', ($(window).height() / 2) - ((logoHome.height() + 30) / 2) + 'px 0px');
+}
+
+
 $(document).ready(function() {
 
-	// Set min height for the main sections
-	var docHeight = $(window).height();
-	$('section.index').css('min-height', docHeight + 'px');
+	setHomeHeight();
+
+	// Set min height for the HOME section, when orientation change
+	$(window).on('orientationchange', function() {
+		setHomeHeight();
+		logoCenterVert();
+	});
+
+
 
 	// Set all backgrounds to inherit full heigth from the parent element
 	$('.background').each(function() {
 		$(this).css('min-height', $(this).parent().height());
 	});
-
-	//  Set the Logo on the Home page to be vertically centered
-	var logoHome = $('section.home').find('div.logo-wrapper');
-	logoHome.css('margin', (docHeight / 2) - ((logoHome.height() + 30) / 2) + 'px 0px');
-
-	// On orientation change, the home page resizes to the size of the screen
 
 });
