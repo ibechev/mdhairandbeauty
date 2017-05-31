cc3c865167e3c7bf024d2dabd840ed9cf4508a73
Ivan Bechev <ivan_bechev@yahoo.com>
2017-05-31 12:43:28 +0100

    ADDED ROOT DIRECTORY CONSTANT
    Defined Root directory constant and used when including header, head
    and footer in the layout

--- webroot/index.php
+++ webroot/index.php
@@ -3,6 +3,7 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 define('VIEWS_PATH', ROOT . DS . 'views');
+define('WEBROOT_PATH', ROOT . DS . 'webroot');
 
 
 require_once(ROOT . DS . 'lib' . DS . 'init.php');
