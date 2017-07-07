66f2a90177eb20cb0d92aee4c1ea49b5c6447e02
Ivan Bechev <ivan_bechev@yahoo.com>
2017-07-07 19:21:40 +0100

    Parallax & Landing elements - Home page
    Parallax & Landing elements - Home page
     - background, logo, text

--- webroot/js/style.js
+++ webroot/js/style.js
@@ -11,9 +11,23 @@ var logoCenterVert = function() {
 	logoHome.css('margin', ($(window).height() / 2) - ((logoHome.height() + 30) / 2) + 'px 0px');
 }
 
+function homeLogoParallax() {
+	$('.home-logo').css({'transform' : 'translateY(0px)', 'opacity' : 0.8});	
+};
+
+function homeTextParallax() {
+	$('.home-text .centerVH p').each(function(i) {
+		setTimeout(function() {
+			$('.home-text .centerVH p').eq(i).addClass('landing');
+		}, 170 * (i+1));
+	});
+};
+
 
 $(document).ready(function() {
 
+	setTimeout(homeLogoParallax, 350);
+	setTimeout(homeTextParallax, 550);
 	setHomeHeight();
 
 	// Set min height for the HOME section, when orientation change
@@ -29,23 +43,32 @@ $(document).ready(function() {
 		$(this).css('min-height', $(this).parent().height());
 	});
 
-	if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {	// Do not show the Scroll to top button on mobile devices
+	$(window).scroll(function()	{
 		
-		$(window).scroll(function() {
+		var wScroll = $(this).scrollTop();
 
-			//************************** 
+		$('.home-logo').css({
+			'transition': 'unset',
+			'transform': 'translate(0px, '+ wScroll / 4.5 +'%)'
+		});
+
+		$('.home-text').css({
+			//'transition': 'unset',
+			'transform': 'translate(0px, '+ wScroll / 5.2 +'%)'
+		});
+
+
+
+		// Check if the device is a mobile devices
+		if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {	
+
 			// Show Scroll to top arrow
-
 			var scrollTopBtn = $('#scroller-top');
-
 			if ($(this).scrollTop() > 350) {
 				scrollTopBtn.fadeIn();
 			} else {
 				scrollTopBtn.fadeOut();
 			}
-
-			//**************************
-
-		});
-	}
+		};
+	});
 });
