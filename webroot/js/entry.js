514ca24423f67772c6b1c2388b837d98e927dd53
Bechev, Ivan <ivan.bechev@accenture.com>
2018-05-21 09:07:56 +0100

    Logo image bright gradient changed. Fixed Heroku - createImageFromJpeg error. Set Gallery JS code to load only on Gallery page

--- webroot/js/entry.js
+++ webroot/js/entry.js
@@ -1,7 +1,8 @@
 // Scripts
 
-require('babel-core/register');
-require('babel-polyfill');
-require('./script');
-require('./navigation');
-require('./galleryPage');
+require("babel-core/register");
+require("babel-polyfill");
+require("./script");
+require("./navigation");
+window.location.pathname === "/mdhairandbeauty/gallery" &&
+  require("./galleryPage");
