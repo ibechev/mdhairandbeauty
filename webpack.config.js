c04849047347d44f4741401454bd46903d7fab45
Bechev, Ivan <ivan.bechev@accenture.com>
2018-05-21 11:24:26 +0100

    Fixing the issue wit production and development path for Gallery page

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname + "/webroot/js",
   devtool: debug ? "inline-sourcemap" : null,
-  entry: ['babel-polyfill', './entry.js'],
+  entry: ["babel-polyfill", "./entry.js"],
   output: {
     path: __dirname + "/webroot/js/build/",
     filename: "scripts.min.js"
   },
   module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader'
-    }]
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader"
+      }
+    ]
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  plugins: debug
+    ? []
+    : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+      ],
   plugins: [
-      new webpack.ProvidePlugin({
+    new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
     })
   ]
-
 };
--- webroot/js/entry.js
+++ webroot/js/entry.js
@@ -1,8 +1,11 @@
 // Scripts
+const galleryPath =
+  process.env.NODE_ENV === "production"
+    ? "/gallery"
+    : "/mdhairandbeauty/gallery";
 
 require("babel-core/register");
 require("babel-polyfill");
 require("./script");
 require("./navigation");
-window.location.pathname === "/mdhairandbeauty/gallery" &&
-  require("./galleryPage");
+window.location.pathname === galleryPath && require("./galleryPage");
