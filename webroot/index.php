f21bc7fd9bdc601a0531a3e000df462dfcb06db1
Bechev, Ivan <ivan.bechev@accenture.com>
2018-05-27 23:07:02 +0100

    Changed PROD environment root from 'mdhairandbeauty.herokuapp.com' to 'mdhairandbeauty.com'.

--- webroot/index.php
+++ webroot/index.php
@@ -6,7 +6,7 @@ define('VIEWS_PATH', ROOT . DS . 'views');
 define('WEBROOT_PATH', ROOT . DS . 'webroot');
 
 // Defining environment url root and setting Global HOST to either localhost or web server host
-define('WEB', 'http://mdhairandbeauty.herokuapp.com');
+define('WEB', 'http://mdhairandbeauty.com');
 define('LOCAL', 'http://localhost/mdhairandbeauty');
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
 	$environment = LOCAL;
@@ -17,6 +17,4 @@ if ($_SERVER['HTTP_HOST'] === 'localhost') {
 require_once(ROOT . DS . 'lib' . DS . 'init.php');
 
 
-App::run(str_replace('/mdhairandbeauty', '', $_SERVER['REQUEST_URI']));	
-
-
+App::run(str_replace('/mdhairandbeauty', '', $_SERVER['REQUEST_URI']));
