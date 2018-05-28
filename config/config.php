a6733e7c2d0f2514476612310278cf9fba68b1db
Bechev, Ivan <ivan.bechev@accenture.com>
2018-05-28 14:55:55 +0100

    SEO - meta tags updated

--- config/config.php
+++ config/config.php
@@ -2,17 +2,23 @@
 
 $GLOBALS['config'] = array(
 
-	'site_name'			=>	'MDhair&beauty',
-	'language' 			=> 	array('en', 'es'),
+	// HTML head
+	'title'	=>	'MD hair & beauty salon | Dedicated to making you look and feel your best!',
+	'description' => 'The beauty-professionals at MD Hair & Beauty Salon are dedicated to making you look and feel your best, every time you walk through our door.',
+	'meta_keywords' => array(
+		'hair', 'haircut', 'beauty', 'skincare', 'manicure', 'makeup', 	'treatment', 'look', 'haircare', 'hairloss', 'care', 'loss', 'colour', 'dyeing', 'diagnostic', 'helthy', 'nail', 'feet', 'body', 'massage', 'kids', 'children', 'scalp', 'premium', 'mallorca', 'palmanova'),
+
+	// Language (not implemented yet)
+	'language' => 	array('en', 'es'),
 
 	// Rroutes. Route name => method prefix
-	'routes'			=> 	array(
+	'routes'	=> 	array(
 				'default'	=>	'',
 				'admin'		=>	'admin_'
 		),
 
 	// Defaults
-	'default_route'		=>	'default',
+	'default_route'	=>	'default',
 	'default_language'	=> 	'en',
 	'default_controller'=>	'home',
 	'default_action'	=>	'index',
@@ -27,12 +33,12 @@ $GLOBALS['config'] = array(
 		),
 
 	'session'			=>	array(
-			'session_name'	=>	'user', 
+			'session_name'	=>	'user',
 			'token_name'	=>	'token'
 		),
 	'remember'			=>	array(
 			'cookie_name'	=>	'hash',
 			'cookie_expiry'	=>	604800
 		)
-	
+
 );
